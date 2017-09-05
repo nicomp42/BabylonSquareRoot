ae248d33822caa9fe52311065e75e42de2aa8e60
Bill Nicholson <nicholdw@ucmail.uc.edu>
2017-09-05 13:44:42 -0400

    Initial Commit with the sqrt method

--- BabylonSquareRoot/Program.cs
+++ BabylonSquareRoot/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BabylonSquareRoot
+namespace babylonSquareRoot
 {
-    class Program
+    class BabylonSquareRoot
     {
         static void Main(string[] args)
         {
+            double num = 49;
+            Console.WriteLine("Square root of " + num + " = " + (new BabylonSquareRoot()). BabylonianSquareRoot(num, .0001));
+        }
+        public double BabylonianSquareRoot(double a, double error)
+        {
+            double sqrt = a;
+            while (Math.Abs(sqrt - a/sqrt) > error) {
+                sqrt = (sqrt + a / sqrt) / 2;
+            }
+            return sqrt;
         }
     }
 }
